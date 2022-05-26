8b0a9f753ef61c7479ca9f4dc07fd41f5ab91ac8
YureAr <nearriver96@gmail.com>
2022-05-26 20:53:29 +0900

    login page and splash screen

---
+++ lib/constants/r.dart
@@ -0,0 +1,10 @@
+import 'package:bootcamp_project/constants/R/app_colors.dart';
+import 'package:bootcamp_project/constants/R/assets.dart';
+import 'package:bootcamp_project/constants/R/strings.dart';
+
+class R {
+  static AppColor colors = AppColor();
+  static Assets assets = Assets();
+  static Strings strings = Strings();
+
+}
---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,27 @@
+import 'dart:async';
+
+import 'package:bootcamp_project/constants/r.dart';
+import 'package:bootcamp_project/view/login_page.dart';
+import 'package:flutter/material.dart';
+
+class SpalshScreen extends StatelessWidget {
+  const SpalshScreen({super.key});
+  static const String route = "splash_screen";
+
+  @override
+  Widget build(BuildContext context) {
+    Timer(const Duration(seconds: 5), () {
+      // Navigator.of(context)
+      // .push(MaterialPageRoute(builder: (context) => LoginPage() ));
+
+      Navigator.of(context).pushNamed(LoginPage.route);
+    });
+
+    return Scaffold(
+      backgroundColor: R.colors.primary,
+      body: Center(
+        child: Image.asset(R.assets.icSplash),
+      ),
+    );
+  }
+}
